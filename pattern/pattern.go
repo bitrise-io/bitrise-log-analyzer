9accfe8b6ef74754cb67243bc4bc4c57f6fa25f8
Viktor Benei <viktor.benei@gmail.com>
2016-10-11 20:05:21 +0200

    multi-line-pattern match handling

--- pattern/pattern.go
+++ pattern/pattern.go
@@ -3,6 +3,7 @@ package pattern
 import (
 	"fmt"
 	"io"
+	"log"
 	"regexp"
 	"strings"
 
@@ -11,23 +12,47 @@ import (
 
 // Model ...
 type Model struct {
-	Line string
+	Lines []string
 }
 
 // Matcher ...
 type Matcher struct {
 	// patterns - should NOT be modified once assigned (through `NewMatcher`)!
 	patterns []Model
+	// patternLineMatchLookup - keeps track of which pattern lines were matched/should be checked next.
+	// The value of this map if the index of the `.Lines` of the related `pattern[idx]`
+	//  which should be checked for a match
+	// map is: index of `patterns` => pattern.Lines index which should match with the next line
+	//  in order to declare the pattern as a match
+	patternLineMatchLookup map[int]int
 	// matches - index of `patterns` which matched
 	matches map[int]bool
 }
 
 // NewMatcher ...
 func NewMatcher(ptrns []Model) Matcher {
-	return Matcher{
-		patterns: ptrns,
-		matches:  map[int]bool{},
+	filteredPatterns := []Model{}
+	filteredPatternLookupMap := map[int]int{}
+
+	filteredPatternsIdx := 0
+	for _, aPattern := range ptrns {
+		if len(aPattern.Lines) < 1 {
+			log.Println("Skipping empty pattern (no Lines defined)")
+			continue
+		}
+		filteredPatterns = append(filteredPatterns, aPattern)
+		// init to pattern.Lines idx 0
+		filteredPatternLookupMap[filteredPatternsIdx] = 0
+		filteredPatternsIdx++
 	}
+
+	matcher := Matcher{
+		patterns:               filteredPatterns,
+		patternLineMatchLookup: filteredPatternLookupMap,
+		matches:                map[int]bool{},
+	}
+
+	return matcher
 }
 
 // Results ...
@@ -48,12 +73,26 @@ func (matcher *Matcher) ProcessLine(line string) error {
 			// skip, already matched
 			continue
 		}
-		r, err := regexp.Compile(aPattern.Line)
+
+		currPatternLineToMatchIdx := matcher.patternLineMatchLookup[idx]
+		patternLineToMatch := aPattern.Lines[currPatternLineToMatchIdx]
+		r, err := regexp.Compile(patternLineToMatch)
 		if err != nil {
 			return fmt.Errorf("Invalid pattern: %s | error: %s", aPattern, err)
 		}
+
 		if r.MatchString(line) {
-			matcher.matches[idx] = true
+			// line matched, check if this was the last in the pattern's Lines array/slice
+			if len(aPattern.Lines)-1 <= currPatternLineToMatchIdx {
+				// if it was, then we just had a match! ;)
+				matcher.matches[idx] = true
+			} else {
+				// otherwise bump the pattern's .Lines index for the next check
+				matcher.patternLineMatchLookup[idx] = currPatternLineToMatchIdx + 1
+			}
+		} else {
+			// line did not match - reset the pattern.Lines index to 0
+			matcher.patternLineMatchLookup[idx] = 0
 		}
 	}
 	return nil
--- pattern/pattern_test.go
+++ pattern/pattern_test.go
@@ -18,36 +18,49 @@ func Test_Matcher_ProcessText(t *testing.T) {
 	t.Log("One liner text - single match")
 	{
 		matcher := NewMatcher([]Model{
-			{Line: "match"},
-			{Line: "no match"},
+			{Lines: []string{"match"}},
+			{Lines: []string{"no match"}},
 		})
 		require.NoError(t, matcher.ProcessText(`this should match`))
-		require.Equal(t, []Model{{Line: "match"}}, matcher.Results())
+		require.Equal(t,
+			[]Model{
+				{Lines: []string{"match"}},
+			},
+			matcher.Results())
 	}
 
 	t.Log("One liner text - single match - same pattern multiple times - result should only include it once")
 	{
 		matcher := NewMatcher([]Model{
-			{Line: "match"},
+			{Lines: []string{"match"}},
 		})
 		require.NoError(t, matcher.ProcessText(`this should match or match and match`))
-		require.Equal(t, []Model{{Line: "match"}}, matcher.Results())
+		require.Equal(t,
+			[]Model{
+				{Lines: []string{"match"}},
+			},
+			matcher.Results())
 	}
 
 	t.Log("One liner text - multi single line match")
 	{
 		matcher := NewMatcher([]Model{
-			{Line: "should"},
-			{Line: "match"},
+			{Lines: []string{"should"}},
+			{Lines: []string{"match"}},
 		})
 		require.NoError(t, matcher.ProcessText(`this should match`))
-		testutil.EqualSlicesWithoutOrder(t, []Model{{Line: "should"}, {Line: "match"}}, matcher.Results())
+		testutil.EqualSlicesWithoutOrder(t,
+			[]Model{
+				{Lines: []string{"should"}},
+				{Lines: []string{"match"}},
+			},
+			matcher.Results())
 	}
 
-	t.Log("Multi liner text - no match")
+	t.Log("Multi-line input text - no match")
 	{
 		matcher := NewMatcher([]Model{
-			{Line: "nothing should match this"},
+			{Lines: []string{"nothing should match this"}},
 		})
 		require.NoError(t, matcher.ProcessText(`first line,
 second line
@@ -55,15 +68,38 @@ and the third line`))
 		testutil.EqualSlicesWithoutOrder(t, []Model{}, matcher.Results())
 	}
 
-	t.Log("Multi liner text - multi single line match")
+	t.Log("Multi-line input text - multi single line match")
 	{
 		matcher := NewMatcher([]Model{
-			{Line: "should"},
-			{Line: "match"},
+			{Lines: []string{"should"}},
+			{Lines: []string{"match"}},
 		})
 		require.NoError(t, matcher.ProcessText(`this should be catched,
 as well as
 this should match`))
-		testutil.EqualSlicesWithoutOrder(t, []Model{{Line: "should"}, {Line: "match"}}, matcher.Results())
+		testutil.EqualSlicesWithoutOrder(t,
+			[]Model{
+				{Lines: []string{"should"}},
+				{Lines: []string{"match"}},
+			},
+			matcher.Results())
+	}
+
+	t.Log("Multi-line input text - multi lines pattern match")
+	{
+		matcher := NewMatcher([]Model{
+			{Lines: []string{
+				"this should",
+				"as well",
+				"match",
+			}},
+		})
+		require.NoError(t, matcher.ProcessText(`this should be catched,
+as well as
+this should match`))
+		testutil.EqualSlicesWithoutOrder(t,
+			[]Model{
+				{Lines: []string{"this should", "as well", "match"}},
+			}, matcher.Results())
 	}
 }
